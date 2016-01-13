343fb7c22b66c38aa9767c7ebc2d5480ccfabd0c
p <koldfuzor@gmail.com>
2016-01-12 16:31:50 -0800

    chess renders fics games

--- chess.cpp
+++ chess.cpp
@@ -25,21 +25,42 @@
 #include "../term/term.h"
 
 namespace LFL {
+DEFINE_string(connect, "freechess.org:5000", "Connect to server");
+
 AssetMap asset;
 SoundAssetMap soundasset;
 Chess::Position position;
+Time move_time;
+int move_square_from = -1, move_square_to = -1;
 bool console_animating = 0;
-DEFINE_string(connect, "freechess.org:5000", "Connect to server");
 
 struct ChessTerminal : public Terminal {
-  using Terminal::Terminal;
-  virtual void Enter() override { sink->WriteChar(Key::CtrlModified('j')); }
+  bool logged_in = false;
+  unsigned char newline = '\n';
+  string erasechar = "\xff\xf7";
+  AhoCorasickFSM<char> login_fsm, move_fsm;
+  StringMatcher<char> login_matcher, move_matcher;
+  ChessTerminal(ByteSink *O, Window *W, Font *F) :
+    Terminal(O, W, F), login_fsm({"\rfics%"}), move_fsm({"\r<12> "}), login_matcher(&login_fsm), move_matcher(&move_fsm) {
+    move_matcher.match_end_condition = &isint<'\r'>;
+  }
+
+  virtual void Enter() override { sink->WriteChar(newline); }
+  virtual void Erase() override { sink->Write(erasechar.data(), erasechar.size()); }
+  virtual void Write(const StringPiece &b, bool update_fb=true, bool release_fb=true) {
+    string s = b.str();
+    if (!logged_in) login_matcher.Match(s);
+    string filtered;
+    move_matcher.Match(s, &filtered);
+    Terminal::Write(filtered, update_fb, release_fb);
+  }
 };
 
-struct ChessTerminalWindow : public TerminalWindow {
+struct ChessTerminalWindow : public TerminalWindowT<ChessTerminal> {
   ChessTerminalWindow(const string &hostport) :
-    TerminalWindow(new NetworkTerminalController(Singleton<HTTPClient>::Get(), hostport)) {
+    TerminalWindowT(new NetworkTerminalController(Singleton<HTTPClient>::Get(), hostport)) {
     controller->frame_on_keyboard_input = true;
+    controller->local_echo = true;
   }
 };
 
@@ -52,14 +73,44 @@ struct ChessGUI : public GUI {
   void Open(const string &hostport) {
     Layout();
     CheckNullAssign(&chess_terminal, new ChessTerminalWindow(hostport));
-    chess_terminal->OpenT<ChessTerminal>(term_dim.x, term_dim.y, FLAGS_default_font_size);
+    chess_terminal->Open(term_dim.x, term_dim.y, FLAGS_default_font_size);
     chess_terminal->terminal->SetColors(Singleton<TextGUI::StandardVGAColors>::Get());
+    chess_terminal->terminal->login_matcher.match_cb = bind(&ChessGUI::LoginCB, this, _1);
+    chess_terminal->terminal->move_matcher.match_cb = bind(&ChessGUI::GameUpdateCB, this, _1);
   }
 
   Box SquareCoords(int p) const {
     static const int border = 5;
     int w = screen->width-2*border, h = screen->height-2*border - term.h;
     return Box(border+Chess::SquareX(p)/8.0*w, border+Chess::SquareY(p)/8.0*h + term.h, 1/8.0*w, 1/8.0*h);
+  }
+
+  void LoginCB(const string&) {
+    unsigned char nl = chess_terminal->terminal->newline;
+    string setup_command = StrCat(nl, "set style 12", nl);
+    chess_terminal->terminal->logged_in = true;
+    chess_terminal->controller->Write(setup_command.data(), setup_command.size());
+  }
+
+  void GameUpdateCB(const string &s) {
+    if (s.size() < 8*9) return;
+    vector<string> args;
+    Split(StringPiece::FromRemaining(s, 8*9), isspace, NULL, &args);
+    position.LoadByteBoard(s, args.size() && args[0] == "B");
+    move_square_from = move_square_to = -1;
+    if (args.size() < 23) return;
+
+    const string &move = args[18];
+    if (move == "o-o") {
+      if (position.move_color) { move_square_from = Chess::e1; move_square_to = Chess::g1; }
+      else                     { move_square_from = Chess::e8; move_square_to = Chess::g8; }
+    } else if (move == "o-o-o") {
+      if (position.move_color) { move_square_from = Chess::e1; move_square_to = Chess::c1; }
+      else                     { move_square_from = Chess::e8; move_square_to = Chess::c8; }
+    } else if (move.size() < 7 || move[1] != '/' || move[4] != '-' ||
+               (move_square_from = Chess::SquareID(&move[2])) == -1 ||
+               (move_square_to   = Chess::SquareID(&move[5])) == -1) return ERROR("Unknown move ", move);
+    move_time = Now();
   }
 
   void Layout() {
@@ -72,6 +123,7 @@ struct ChessGUI : public GUI {
   }
 
   int Frame(LFL::Window *W, unsigned clicks, int flag) {
+    Time now = Now();
     chess_terminal->ReadAndUpdateTerminalFramebuffer();
     Draw();
 
@@ -80,6 +132,12 @@ struct ChessGUI : public GUI {
     for (int i=Chess::PAWN; i <= Chess::KING; i++) {
       Bit::Indices(position.white[i], bits); for (int *b = bits; *b != -1; b++) { Box w=SquareCoords(*b); pieces->DrawGlyph(black_font_index[i]+6, w); }
       Bit::Indices(position.black[i], bits); for (int *b = bits; *b != -1; b++) { Box w=SquareCoords(*b); pieces->DrawGlyph(black_font_index[i],   w); }
+    }
+
+    if (move_square_from != -1 && move_square_to != -1) {
+      screen->gd->SetColor(Color(85, 85,  255)); BoxOutline(1).Draw(SquareCoords(move_square_from));
+      screen->gd->SetColor(Color(85, 255, 255)); BoxOutline(1).Draw(SquareCoords(move_square_to));
+      screen->gd->SetColor(Color::white);
     }
 
     W->gd->DisableBlend();
@@ -127,8 +185,10 @@ extern "C" int main(int argc, const char *argv[]) {
   app->shell.soundassets = &soundasset;
 
   BindMap *binds = screen->binds = new BindMap();
-  binds->Add(Bind('6', Key::Modifier::Cmd, Bind::CB(bind(&Shell::console,         &app->shell, vector<string>()))));
-  binds->Add(Bind(Key::Escape,             Bind::CB(bind(&Shell::quit,            &app->shell, vector<string>()))));
+  binds->Add(Bind('6',       Key::Modifier::Cmd, Bind::CB(bind(&Shell::console, &app->shell, vector<string>()))));
+  binds->Add(Bind(Key::Escape,                   Bind::CB(bind(&Shell::quit,    &app->shell, vector<string>()))));
+  binds->Add(Bind(Key::Up,   Key::Modifier::Cmd, Bind::CB(bind([=](){ chess_gui->chess_terminal->ScrollHistory(1); }))));
+  binds->Add(Bind(Key::Down, Key::Modifier::Cmd, Bind::CB(bind([=](){ chess_gui->chess_terminal->ScrollHistory(0); }))));
 
   chess_gui->Open(FLAGS_connect);
   return app->Main();
